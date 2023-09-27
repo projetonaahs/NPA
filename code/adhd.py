83e8a2e0e6636bf9db31bae68b1650fda5bee886
Julia <jmoraeslia@gmail.com>
2023-09-27 14:15:41 -0300

    connections

--- code/adhd.py
+++ code/adhd.py
@@ -1,6 +1,5 @@
-from nilearn import datasets, plotting, maskers, connectome
+from nilearn import datasets, maskers, connectome
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 data = datasets.fetch_adhd(n_subjects=10)
@@ -19,25 +18,34 @@ m4sk3r = maskers.NiftiMapsMasker(
 m4sk3r.fit(func_file)
 t_series = m4sk3r.transform(func_file, confounds=confounds_file)
 
-# calcula a matriz de conectividade funcional.
+# Calcula a matriz de conectividade funcional.
 c_measure = connectome.ConnectivityMeasure(kind='correlation')
 c_matrix = c_measure.fit_transform([t_series])[0]
 
-# preenche de branco a diagonal da matriz e plota. - evitar equívocos.
+# Preenche de branco a diagonal da matriz
 np.fill_diagonal(c_matrix, 0)
-plotting.plot_matrix(
-    c_matrix, labels=labels, colorbar=True, vmax=0.8, vmin=-0.8
-)
 
-# plota o gráfico de connectome - apenas 15%.
-coords = a.region_coords
-plotting.plot_connectome(
-    c_matrix, coords, edge_threshold='85%'
-)
+# Configuração da figura para a matriz de correlação
+fig_matrix, ax_matrix = plt.subplots(figsize=(8, 6))
+cax_matrix = ax_matrix.matshow(c_matrix, cmap='coolwarm', vmax=0.8, vmin=-0.8)
+fig_matrix.colorbar(cax_matrix, shrink=0.8, aspect=20)
+ax_matrix.set_title("Matriz de Correlação")
 
-# Exibir as partes do cérebro envolvidas na análise
-print("Rótulos das Regiões e Coordenadas do Atlas MSDL:")
-for label, coord in zip(labels, coords):
-    print(f"Região: {label}, Coordenadas: {coord}")
+# Configuração dos rótulos das regiões
+ax_matrix.set_xticks(np.arange(len(labels)))
+ax_matrix.set_yticks(np.arange(len(labels)))
+ax_matrix.set_xticklabels(labels, rotation=90)
+ax_matrix.set_yticklabels(labels)
 
-plotting.show()
+# Exibe as partes do cérebro envolvidas na análise
+
+# Cria um gráfico vazio apenas para os rótulos e força
+fig_legend, ax_legend = plt.subplots(figsize=(6, 6))
+ax_legend.axis('off')
+
+# Calcula e configura as forças das conexões
+legend_text = "\n".join([f"{label1} + {label2}: {c_matrix[i, j]:.2f}" for i, label1 in enumerate(labels) for j, label2 in enumerate(labels) if i < j])
+ax_legend.text(0.1, 0.1, legend_text, fontsize=10)
+
+# Exibe a primeira janela com a matriz de correlação e a segunda janela com os rótulos e a força das conexões
+plt.show()
